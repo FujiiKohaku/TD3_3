9f5cf8d1cbc8dca4427ce43b38dfebd46d9c4887
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-02-04 23:35:29 +0900

    komito

--- Scene/ResultScene.cpp
+++ Scene/ResultScene.cpp
@@ -167,14 +167,10 @@ void ResultScene::Draw3D()
     skydome_->Draw();
 }
 
-#ifdef USE_IMGUI
-
-
-
 void ResultScene::DrawImGui()
 {
+#ifdef USE_IMGUI
     ImGui::Text("Perfect : %d", perfectCount_);
     ImGui::Text("Good : %d", goodCount_);
-}
-
 #endif // DEBUG
+}
